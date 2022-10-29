79e8bc8347f658c734e73eae0885dc2d3e86f2d7
Greg <grzegorz.karol.jasinski@gmail.com>
2022-10-29 09:01:10 +0200

    local utl

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,92 +13,74 @@ import (
 // Variables used for command line parameters
 var (
 	Token   string
+	AppId   string
+	GuildId string
 	GifHost string
 	OutUrl  string = "http://srv12.mikr.us:30472"
 )
 
 func init() {
-
 	flag.StringVar(&Token, "t", "NzQxMDY3NzYzODQ5NjI1NjUx.GG8h-D.ZFbbVO0hIjBSG3Q-PbZStOG8vbWhmoyIi2D4cc", "Bot Token")
-	flag.StringVar(&GifHost, "g", "localhost:30472", "The URL of the GIF app")
+	flag.StringVar(&GifHost, "g", "http://localhost:30472", "The URL of the GIF app")
+	flag.StringVar(&AppId, "a", "", "Discord APP ID")
+	flag.StringVar(&GuildId, "s", "", "Discord Guild ID (Server ID)")
 	flag.Parse()
 }
 
 func main() {
 
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
+
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	// create commands
+	_, err = dg.ApplicationCommandBulkOverwrite(AppId, GuildId, []*discordgo.ApplicationCommand{
+		{
+			Name:        "saily",
+			Description: "Creates the saily game",
+		},
+		{
+			Name:        "say",
+			Description: "Outputs animated paszczak GIF",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "git-user-name",
+					Description: "Name of the GitHub user account",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "message",
+					Description: "Message to put in the bubble",
+					Required:    true,
+				},
+			},
+		},
+	})
 
-	// In this example, we only care about receiving message events.
+	// Handle the error of creating commands
+	if err != nil {
+		fmt.Println("Unable to create server commands")
+		return
+	}
+
+	dg.AddHandler(sailyCommands)
+	dg.AddHandler(sayCommand)
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
-	if err != nil {
+	if err := dg.Open(); err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	dg.Close()
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	if !strings.HasPrefix(m.Content, "say#") {
-		return
-	}
-
-	msg := strings.Split(strings.ReplaceAll(m.Content, "\n", " "), "say#")
-
-	fmt.Println(msg)
-	fmt.Println(len(msg))
-
-	if len(msg) != 2 {
-		return
-	}
-
-	bas := GifHost + "/czo?gituser=greg-develtio&msg="
-	bas_out := OutUrl + "/czo?gituser=greg-develtio&msg="
-	gif_msg := url.QueryEscape(msg[1])
-
-	response, err := http.Get(bas + gif_msg)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		return
-	}
-
-	_, err = s.ChannelFileSend(m.ChannelID, "grzybotsays.gif", response.Body)
-
-	if err != nil {
-		return
-	}
-	// s.ChannelMessageSend(m.ChannelID, bas+gif_msg)
-	s.ChannelMessageSend(m.ChannelID, "`"+bas_out+gif_msg+"`")
 }
